bec26cf2eb50cb8a46e879f6f18d351a9cad3091
Brenda Flores <kmietotwy@gmail.com>
2020-01-01 10:07:01 -0800

    using IEnumerables to iterate through collections

---
+++ Features/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Employee> developers = new Employee[]
+            {
+                new Employee {Id = 1, Name = "Brenda"},
+                new Employee {Id = 2, Name = "Marian"}
+            };
+
+            IEnumerable<Employee> sales = new List<Employee>
+            {
+                new Employee {Id = 3, Name = "Sally"}
+            };
+
+            IEnumerator<Employee> enumerator = sales.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Name);
+            }
+        }
+    }
+}
