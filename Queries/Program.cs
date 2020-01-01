1ca62be094800ceaba20f137414b1c34f42dbe53
Brenda Flores <kmietotwy@gmail.com>
2020-01-01 11:31:22 -0800

    created static random method to portray benefit of streaming operators when working with theoretically infinitely long datasets.  we write an infinite loop in the Random method.  in Program, we invoke Random but use LINQ .Where() as a deferred streaming operation and .Take() to stop the loop.

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -8,6 +8,13 @@ namespace Queries
     {
         static void Main(string[] args)
         {
+            var numbers = MyLinq.Random().Where(n => n > 0.5).Take(10);
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+
             var movies = new List<Movie>
             {
                 new Movie {Title = "Spirited Away", Rating = 8.5f, Year = 2001},
@@ -17,14 +24,13 @@ namespace Queries
                 new Movie {Title = "My Neighbor Totoro", Rating = 8.1f, Year = 1988}
             };
 
-            var query = movies.Filter(m => m.Year > 2000);
-            Console.WriteLine(query.Count());
+            var query = movies.Where(m => m.Year > 2000).OrderByDescending(m => m.Rating);
 
             var enumerator = query.GetEnumerator();
-            while (enumerator.MoveNext())
-            {
-                Console.WriteLine(enumerator.Current.Title);
-            }
+            //while (enumerator.MoveNext())
+            //{
+            //    Console.WriteLine(enumerator.Current.Title);
+            //}
         }
     }
 }
