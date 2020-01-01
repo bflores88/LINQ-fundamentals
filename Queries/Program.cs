581490d905b8fb32125e910be452922837c1d2f9
Brenda Flores <kmietotwy@gmail.com>
2020-01-01 10:56:31 -0800

    created Filter extenstion method to mimic LINQ Where

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -17,7 +17,7 @@ namespace Queries
                 new Movie {Title = "My Neighbor Totoro", Rating = 8.1f, Year = 1988}
             };
 
-            var query = movies.Where(m => m.Year > 2000);
+            var query = movies.Filter(m => m.Year > 2000);
 
             foreach (var movie in query)
             {
