8fb9570e2e9b1139743530c8c8d5478a9a6bff98
Brenda Flores <kmietotwy@gmail.com>
2020-01-01 12:34:26 -0800

    wrote LINQ method syntax query to top 10 cars with best combined mileage

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -10,9 +10,11 @@ namespace Cars
         static void Main(string[] args)
         {
             var cars = ProcessFile("fuel.csv");
-            foreach (var car in cars)
+
+            var query = cars.OrderByDescending(c => c.Combined).ThenBy(c => c.Name);
+            foreach (var car in query.Take(10))
             {
-                Console.WriteLine(car.Name);
+                Console.WriteLine($"{car.Name}: {car.Combined}");
             }
         }
 
