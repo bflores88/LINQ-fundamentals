d2432f8cc5b03efcc90fe3c656594724d3eb5d41
Brenda Flores <kmietotwy@gmail.com>
2020-01-01 10:39:16 -0800

    comparing LINQ query syntax and method syntax

--- Features/Program.cs
+++ Features/Program.cs
@@ -17,8 +17,8 @@ namespace Features
 
             IEnumerable<Employee> developers = new Employee[]
             {
-                new Employee {Id = 1, Name = "Brenda"},
-                new Employee {Id = 2, Name = "Marian"}
+                new Employee {Id = 1, Name = "Scott"},
+                new Employee {Id = 2, Name = "Chris"}
             };
 
             IEnumerable<Employee> sales = new List<Employee>
@@ -26,7 +26,15 @@ namespace Features
                 new Employee {Id = 3, Name = "Sally"}
             };
 
-            foreach (var employee in developers.Where(e => e.Name.StartsWith("B")))
+            var query = developers.Where(e => e.Name.Length == 5)
+                .OrderBy(e => e.Name);
+
+            var query2 = from developer in developers
+                where developer.Name.Length == 5
+                orderby developer.Name
+                select developer;
+
+            foreach (var employee in query2)
             {
                 Console.WriteLine(employee.Name);
             }
