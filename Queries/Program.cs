a6d5aa673b63828bfd77b59928e788b453d98db5
Brenda Flores <kmietotwy@gmail.com>
2020-01-01 10:52:14 -0800

    created a new project Queries with class Movie.  implemented a LINQ query syntax to log the titles of movies made after 2000

---
+++ Queries/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var movies = new List<Movie>
+            {
+                new Movie {Title = "Spirited Away", Rating = 8.5f, Year = 2001},
+                new Movie {Title = "Your Name.", Rating = 8.5f, Year = 2016},
+                new Movie {Title = "Princess Mononoke", Rating = 8.4f, Year = 1997},
+                new Movie {Title = "A Silent Voice", Rating = 8.3f, Year = 2016},
+                new Movie {Title = "My Neighbor Totoro", Rating = 8.1f, Year = 1988}
+            };
+
+            var query = movies.Where(m => m.Year > 2000);
+
+            foreach (var movie in query)
+            {
+                Console.WriteLine(movie.Title);
+            }
+        }
+    }
+}
