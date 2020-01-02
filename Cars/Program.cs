4454ed459317c93d562a00a5e99f9a0b1e8311df
Brenda Flores <kmietotwy@gmail.com>
2020-01-02 09:01:51 -0800

    challenge: join two datasets on manufacturer name, group by country, and log top 3 most fuel efficient

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -16,24 +16,27 @@ namespace Cars
                 from manufacturer in manufacturers
                 join car in cars on manufacturer.Name equals car.Manufacturer
                     into carGroup
-                orderby manufacturer.Name
                 select new
                 {
                     Manufacturer = manufacturer,
                     Cars = carGroup
-                };
+                } into result
+                group result by result.Manufacturer.Headquarters;
 
             var query2 =
                 manufacturers.GroupJoin(cars, m => m.Name, c => c.Manufacturer, (m, g) => new
-                {
-                    Manufacturer = m,
-                    Cars = g
-                }).OrderBy(m => m.Manufacturer.Name);
+                    {
+                        Manufacturer = m,
+                        Cars = g
+                    })
+                    .GroupBy(m => m.Manufacturer.Headquarters);
 
             foreach (var group in query2)
             {
-                Console.WriteLine($"{group.Manufacturer.Name} : {group.Manufacturer.Headquarters}");
-                foreach (var car in group.Cars.OrderByDescending(c => c.Combined).Take(2))
+                Console.WriteLine(group.Key);
+                foreach (var car in group.SelectMany(g => g.Cars)
+                                        .OrderByDescending(c => c.Combined)
+                                        .Take(3))
                 {
                     Console.WriteLine($"\t{car.Name} : {car.Combined}");
                 }
