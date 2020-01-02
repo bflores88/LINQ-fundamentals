83bd751966b669802041a1bae119ab360348fde4
Brenda Flores <kmietotwy@gmail.com>
2020-01-02 08:18:27 -0800

    writing a Join query witht LINQ query syntax

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -9,14 +9,17 @@ namespace Cars
     {
         static void Main(string[] args)
         {
-            var cars = ProcessFile("fuel.csv");
+            var cars = ProcessCars("fuel.csv");
+            var manufacturers = ProcessManufacturers("manufacturers.csv");
 
             var query = 
                 from car in cars
+                join manufacturer in manufacturers 
+                    on car.Manufacturer equals manufacturer.Name
                 orderby car.Combined descending , car.Name ascending 
                 select  new
                 {
-                    car.Manufacturer,
+                    manufacturer.Headquarters,
                     car.Name,
                     car.Combined
                 };
@@ -25,19 +28,37 @@ namespace Cars
                 cars.SelectMany(c => c.Name)
                     .OrderBy(c => c);
 
-            foreach (var character in result)
-            {
-                Console.WriteLine(character);
-            }
-
-
-            //foreach (var car in query.Take(10))
+            //foreach (var character in result)
             //{
-            //    Console.WriteLine($"{car.Name}: {car.Combined}");
+            //    Console.WriteLine(character);
             //}
+
+
+            foreach (var car in query.Take(10))
+            {
+                Console.WriteLine($"{car.Headquarters} {car.Name}: {car.Combined}");
+            }
         }
 
-        private static List<Car> ProcessFile(string path)
+        private static List<Manufacturer> ProcessManufacturers(string path)
+        {
+            var query =
+                File.ReadAllLines(path)
+                    .Where(l => l.Length > 1)
+                    .Select(l =>
+                    {
+                        var columns = l.Split(',');
+                        return new Manufacturer
+                        {
+                            Name = columns[0],
+                            Headquarters = columns[1],
+                            Year = int.Parse(columns[2])
+                        };
+                    });
+            return query.ToList();
+        }
+
+        private static List<Car> ProcessCars(string path)
         {
             var query =
                 File.ReadAllLines(path)
