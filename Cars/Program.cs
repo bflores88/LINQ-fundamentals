f02d1f4a87948a9a5334f90d0e404f2b9284aa7b
Brenda Flores <kmietotwy@gmail.com>
2020-01-02 08:39:02 -0800

    writing queries for grouping

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -12,44 +12,23 @@ namespace Cars
             var cars = ProcessCars("fuel.csv");
             var manufacturers = ProcessManufacturers("manufacturers.csv");
 
-            var query = 
+            var query =
                 from car in cars
-                join manufacturer in manufacturers 
-                    on new { car.Manufacturer, car.Year } 
-                    equals new { Manufacturer = manufacturer.Name, manufacturer.Year }
-                orderby car.Combined descending , car.Name ascending 
-                select  new
-                {
-                    manufacturer.Headquarters,
-                    car.Name,
-                    car.Combined
-                };
+                group car by car.Manufacturer.ToUpper() into manufacturer
+                orderby manufacturer.Key
+                select manufacturer;
 
             var query2 =
-                cars.Join(manufacturers,
-                        c => new { c.Manufacturer, c.Year },
-                        m => new { Manufacturer = m.Name, m.Year }, (c, m) => new
-                        {
-                            m.Headquarters,
-                            c.Name,
-                            c.Combined
-                        })
-                    .OrderByDescending(c => c.Combined)
-                    .ThenBy(c => c.Name);
+                cars.GroupBy(c => c.Manufacturer.ToUpper())
+                    .OrderBy(g => g.Key);
 
-            var result =
-                cars.SelectMany(c => c.Name)
-                    .OrderBy(c => c);
-
-            //foreach (var character in result)
-            //{
-            //    Console.WriteLine(character);
-            //}
-
-
-            foreach (var car in query.Take(10))
+            foreach (var group in query2)
             {
-                Console.WriteLine($"{car.Headquarters} {car.Name}: {car.Combined}");
+                Console.WriteLine(group.Key);
+                foreach (var car in group.OrderByDescending(c => c.Combined).Take(2))
+                {
+                    Console.WriteLine($"\t{car.Name} : {car.Combined}");
+                }
             }
         }
 
