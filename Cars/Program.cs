802da9054f375802bdc653a75d02a534b7a47801
Brenda Flores <kmietotwy@gmail.com>
2020-01-02 07:55:20 -0800

    created a LINQ method ToCar() and abstracted method to convert line to a car object from Car class

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -13,8 +13,15 @@ namespace Cars
 
             var query = 
                 from car in cars
-                orderby car.Combined ascending , car.Name ascending 
+                where car.Manufacturer == "BMW" && car.Year == 2016
+                orderby car.Combined descending , car.Name ascending 
                 select  car;
+
+            var result =
+                cars.All(c => c.Manufacturer == "Ford");
+
+            Console.WriteLine(result);
+
 
             foreach (var car in query.Take(10))
             {
@@ -25,11 +32,41 @@ namespace Cars
         private static List<Car> ProcessFile(string path)
         {
             var query =
-                from line in File.ReadAllLines(path).Skip(1)
-                where line.Length > 1
-                select Car.ParseFromCsv(line);
+                File.ReadAllLines(path)
+                    .Skip(1)
+                    .Where(l => l.Length > 1)
+                    .ToCar();
+
+
+                //from line in File.ReadAllLines(path).Skip(1)
+                //where line.Length > 1
+                //select Car.ParseFromCsv(line);
 
             return query.ToList();
+        }
+    }
+
+    public static class CarExtensions
+    {
+        public static IEnumerable<Car> ToCar(this IEnumerable<string> lines)
+        {
+            foreach (var line in lines)
+            {
+                var columns = line.Split(',');
+
+                yield return new Car
+                {
+                    Year = int.Parse(columns[0]),
+                    Manufacturer = columns[1],
+                    Name = columns[2],
+                    Displacement = double.Parse(columns[3]),
+                    Cylinders = int.Parse(columns[4]),
+                    City = int.Parse(columns[5]),
+                    Highway = int.Parse(columns[6]),
+                    Combined = int.Parse(columns[7])
+                };
+            }
+           
         }
     }
 }
